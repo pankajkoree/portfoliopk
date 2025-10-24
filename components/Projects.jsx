b2e0591e77f8e9938e8036dd591097f413a440db
pankajkoree <pankajkori2057@gmail.com>
2025-10-24 22:27:13 +0545

    project updated

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,80 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Projects = () => {
+  const handleCurrencyConvertor = () => {
+    window.open(
+      "https://currencyconvertorreactapp.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleShoe = () => {
+    window.open(
+      "https://theshoecompany-tailwind.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleMovie = () => {
+    window.open(
+      "https://moviesearchappomdb.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleFocus = () => {
+    window.open(
+      "https://focusontoday-tan.vercel.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleCountry = () => {
+    window.open(
+      "https://countryproject-manual-deploy.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleBurger = () => {
+    window.open(
+      "https://hamburgermenuappnweb.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleWeatherApp = () => {
+    window.open(
+      "https://weatherappchallengejs.netlify.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleTesla = () => {
+    window.open(
+      "https://dashboard-tesla-ui.vercel.app/dashboard",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handleEcommerce = () => {
+    window.open(
+      "https://ecommerce-frontend-products.vercel.app/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
   return (
     <div className="relative flex flex-col w-full">
       {/* Navbar heading */}
@@ -10,8 +83,55 @@ const Projects = () => {
 
       {/* projects */}
       <div className="relative flex flex-col">
+        {/* fullstack project */}
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleEcommerce}
+        >
+          {/* title and navigation */}
+          <div className="relative flex w-full justify-between">
+            <div className="font-semibold">
+              <p>Ecommerce Products API</p>
+            </div>
+            <div className="relative flex gap-4">
+              <Link
+                href="https://github.com/pankajkoree/ecommerce-frontend"
+                target="_blank"
+              >
+                <p className="hover:text-cyan-600">Github </p>
+              </Link>
+              <Link
+                href="https://ecommerce-frontend-products.vercel.app/"
+                target="_blank"
+              >
+                <p className="hover:text-cyan-600">Live </p>
+              </Link>
+            </div>
+          </div>
+          {/* end title and navigation */}
+          {/* description and image */}
+          <div className="ralative flex items-center">
+            <p>
+              Develop, Build, and Test your front-end instantly with our dummy
+              e-commerce API. Get ready-to-use JSON data for products, users,
+              carts, and more — no backend setup required.
+            </p>
+            <Image
+              src="/products.png"
+              alt="currency convertor"
+              width={80}
+              height={80}
+            />
+          </div>
+          {/* end description and image */}
+        </div>
+        {/* end fullstack project */}
+
         {/* project 1 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleCurrencyConvertor}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -51,7 +171,10 @@ const Projects = () => {
         {/* end project 1 */}
 
         {/* project 2 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleShoe}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -93,7 +216,10 @@ const Projects = () => {
         {/* end project 2 */}
 
         {/* project 3 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleMovie}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -135,7 +261,10 @@ const Projects = () => {
         {/* end project 3 */}
 
         {/* project 4 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleFocus}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -174,7 +303,10 @@ const Projects = () => {
         {/* end project 4 */}
 
         {/* project 5 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleCountry}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -216,7 +348,10 @@ const Projects = () => {
         {/* end project 5 */}
 
         {/* project 6 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleBurger}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -258,7 +393,10 @@ const Projects = () => {
         {/* end project 6 */}
 
         {/* project 7 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleWeatherApp}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
@@ -300,7 +438,10 @@ const Projects = () => {
         {/* end project 7 */}
 
         {/* project 8 */}
-        <div className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2">
+        <div
+          className="relative flex flex-col p-2 transition-transform duration-300 ease-in-out hover:translate-x-2 hover:cursor-pointer"
+          onClick={handleTesla}
+        >
           {/* title and navigation */}
           <div className="relative flex w-full justify-between">
             <div className="font-semibold">
